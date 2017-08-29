717df20fdbbe84a0cab99f128f46190d12bab078
Ray <vbisbest@yahoo.com>
2017-08-29 13:06:39 -0400

    really added core.ui

---
+++ GUI/core.ui/Extensions.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace System
+{
+    public static class MoreGrid
+    {
+        public static void Add(this Grid source, ColumnDefinition cd, RowDefinition rd, UIElement uie)
+        {
+            if (source != null)
+            {
+                if (uie != null)
+                {
+                    if (!source.Children.Contains(uie))
+                    {
+                        source.Children.Add(uie);
+                    }
+
+                    if (cd != null)
+                    {
+                        Grid.SetColumn(uie, source.IndexOf(cd));
+                    }
+
+                    if (rd != null)
+                    {
+                        Grid.SetRow(uie, source.IndexOf(rd));
+                    }
+                }
+            }
+        }
+
+        public static int IndexOf(this Grid source, ColumnDefinition cd)
+        {
+            int rc = -1;
+
+            if (source != null)
+            {
+                if (cd != null)
+                {
+                    int index = -1;
+                    foreach (ColumnDefinition column in source.ColumnDefinitions)
+                    {
+                        index++;
+
+                        if (column == cd)
+                        {
+                            rc = index;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return rc;
+        }
+
+        public static int IndexOf(this Grid source, RowDefinition rd)
+        {
+            int rc = -1;
+
+            if (source != null)
+            {
+                if (rd != null)
+                {
+                    int index = -1;
+                    foreach (RowDefinition row in source.RowDefinitions)
+                    {
+                        index++;
+
+                        if (row == rd)
+                        {
+                            rc = index;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return rc;
+        }
+    }
+
+    public static class MoreString
+    {
+        public static Color ToColor(this string source)
+        {
+            Color rc = Colors.Transparent;
+
+
+            try
+            {
+                rc = Color.FromRgb((byte)int.Parse(source.Substring(0, 2), NumberStyles.HexNumber), (byte)int.Parse(source.Substring(2, 2), NumberStyles.HexNumber), (byte)int.Parse(source.Substring(4, 2), NumberStyles.HexNumber));
+            }
+            catch
+            {
+            }
+
+            return rc;
+        }
+    }
+}
---
+++ GUI/core.ui/TransactableList.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core.ui
+{
+    public enum UITransactionOp
+    {
+        InsertAt,
+        Add,
+        AddRange,
+        RemoveAt,
+        RemoveRange,
+        SetAt,
+        Clear,
+        Checkpoint
+    }
+    public class UITransaction
+    {
+        public UITransactionOp op;
+        public int i1;
+        public int i2;
+        public object[] elems;
+        public IUITransactable context;
+    }
+    public class UITransactionManager
+    {
+        List<UITransaction> trans = new List<UITransaction>();
+        int pos=0;
+
+        public UITransactionManager()
+        {
+            Enabled = true;
+        }
+        public void Transact(IUITransactable context, UITransactionOp op, object[] elems)
+        {
+            if (!Enabled)
+                return;
+            checkRestart();
+            UITransaction tr = new UITransaction();
+            tr.op=op;
+            tr.elems = elems;
+            tr.context=context;
+            trans.Add(tr);
+        }
+        public void Transact(IUITransactable context, UITransactionOp op, int i1, object[] elems)
+        {
+            if (!Enabled)
+                return;
+            checkRestart();
+            UITransaction tr = new UITransaction();
+            tr.op = op;
+            tr.i1 = i1;
+            tr.elems = elems;
+            tr.context=context;
+            trans.Add(tr);
+        }
+        public void Transact(IUITransactable context, UITransactionOp op, int i1, int i2, object[] elems)
+        {
+            if (!Enabled)
+                return;
+            checkRestart();
+            UITransaction tr = new UITransaction();
+            tr.op = op;
+            tr.i1 = i1;
+            tr.i2 = i2;
+            tr.elems = elems;
+            tr.context=context;
+            trans.Add(tr);
+        }
+
+        private void checkRestart()
+        {
+            if(pos!=0)
+            {
+                trans.RemoveRange(trans.Count - pos, pos);
+                pos = 0;
+            }
+        }
+
+        public void Undo()
+        {
+            while (pos < trans.Count)
+            {
+                var tt = trans[trans.Count - 1 - pos];
+                if (tt.op == UITransactionOp.Checkpoint)
+                {
+                    pos++;
+                    break;
+                }
+                tt.context.UndoTransaction(tt);
+                pos++;
+            }
+        }
+        public void Redo()
+        {
+            while (pos >= 0)
+            {
+                pos--;
+                var tt = trans[trans.Count - 1 - pos];
+                if(tt.op== UITransactionOp.Checkpoint)
+                {
+                    pos--;
+                    break;
+                }
+                tt.context.PlayTransaction(tt);
+            }
+        }
+        public void Clear()
+        {
+            trans.Clear();
+        }
+        public void Commit()
+        {
+            if (!Enabled)
+                return;
+            checkRestart();
+            if (trans.Count > 0 && trans[trans.Count - 1].op == UITransactionOp.Checkpoint)
+                return;
+            UITransaction tr = new UITransaction();
+            tr.op = UITransactionOp.Checkpoint;
+            trans.Add(tr);
+        }
+        public bool Enabled
+        {
+            get;
+            set;
+        }
+    }
+    public interface IUITransactable
+    {
+        void PlayTransaction(UITransaction tr);
+        void UndoTransaction(UITransaction tr);
+    }
+    public class UITransactableList<T> : IUITransactable where T : class
+    {
+        List<T> list = new List<T>(); // internal copy
+        UITransactionManager tm;
+
+        public UITransactableList(UITransactionManager tm)
+        {
+            this.tm = tm;
+        }
+        public T this[int index]
+        {
+            get { return list[index]; }
+            set
+            {
+                tm.Transact(this,UITransactionOp.SetAt, index, new T[] { list[index], value });
+                list[index] = value;
+            }
+        }
+        public List<T> InternalList
+        {
+            get { return list; }
+        }
+        public void PlayTransaction(UITransaction tr)
+        {
+            switch(tr.op)
+            {
+                case UITransactionOp.SetAt :
+                    list[tr.i1] = (T)tr.elems[1];
+                    break;
+                case UITransactionOp.Add :
+                    list.Add((T)tr.elems[0]);
+                    break;
+                case UITransactionOp.AddRange :
+                    foreach (var obj in tr.elems)
+                        list.Add((T)obj);
+                    break;
+                case UITransactionOp.RemoveAt :
+                    list.RemoveAt(tr.i1);
+                    break;
+                case UITransactionOp.RemoveRange :
+                    list.RemoveRange(tr.i1, tr.i2);
+                    break;
+                case UITransactionOp.Clear :
+                    list.Clear();
+                    break;
+                case UITransactionOp.InsertAt :
+                    list.Insert(tr.i1, (T)tr.elems[0]);
+                    break;
+                default :
+                    throw new NotImplementedException("playtrans Unsupported OP: " + tr.op);
+            }
+        }
+
+        public void UndoTransaction(UITransaction tr)
+        {
+            int ix;
+            switch (tr.op)
+            {
+                case UITransactionOp.SetAt :
+                    list[tr.i1] = (T)tr.elems[0];
+                    break;
+                case UITransactionOp.Add :
+                    list.RemoveAt(list.Count - 1);
+                    break;
+                case UITransactionOp.AddRange :
+                    list.RemoveRange(list.Count - tr.i1, tr.i1);
+                    break;
+                case UITransactionOp.RemoveAt :
+                    list.Insert(tr.i1, (T)tr.elems[0]);
+                    break;
+                case UITransactionOp.RemoveRange :
+                    for (ix = 0; ix < tr.elems.Length;ix++ )
+                    {
+                        list.Insert(tr.i1 + ix, (T)tr.elems[ix]);
+                    }
+                    break;
+                case UITransactionOp.Clear :
+                    foreach (var obj in tr.elems)
+                    {
+                        list.Add((T)obj);
+                    }
+                    break;
+                case UITransactionOp.InsertAt :
+                    list.RemoveAt(tr.i1);
+                    break;
+                default:
+                    throw new NotImplementedException("undotrans Unsupported OP: "+tr.op);
+            }
+        }
+
+        public void Clear()
+        {
+            tm.Transact(this,UITransactionOp.Clear, list.ToArray());
+            list.Clear();
+        }
+        public void Add(T obj)
+        {
+            tm.Transact(this,UITransactionOp.Add, new T[] { obj });
+            list.Add(obj);
+        }
+        public void AddRange(IEnumerable<T> set)
+        {
+            List<T> temp = new List<T>();
+            temp.AddRange(set);
+            tm.Transact(this,UITransactionOp.AddRange, temp.Count, temp.ToArray());
+            list.AddRange(set);
+        }
+        public void Insert(int idx,T obj)
+        {
+            tm.Transact(this,UITransactionOp.InsertAt, idx,new T[] { obj });
+            list.Insert(idx, obj);
+        }
+        public void RemoveAt(int idx)
+        {
+            tm.Transact(this,UITransactionOp.RemoveAt, idx, new T[] { list[idx] });
+            list.RemoveAt(idx);
+        }
+        public void Remove(T obj)
+        {
+            int index = list.IndexOf(obj);
+            if (index < 0)
+                return;
+            RemoveAt(index);
+        }
+        public int Count
+        {
+            get { return list.Count; }
+        }
+        public void RemoveRange(int index,int count)
+        {
+            var r = list.GetRange(index, count);
+            tm.Transact(this,UITransactionOp.RemoveRange, index, count, r.ToArray() );
+            list.RemoveRange(index, count);
+        }
+    }
+
+}
